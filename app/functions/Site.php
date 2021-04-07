bf398c72fcf68e4d0a9251a06b30b75004c5d695
Björn Schleyer <68195428+BSchleyer@users.noreply.github.com>
2021-04-07 23:31:32 +0200

    Update Site.php

--- app/functions/Site.php
+++ app/functions/Site.php
@@ -140,6 +140,31 @@ class Site extends Controller
             return null;
         }
     }
+    
+    public function validateCaptcha($h_captcha_response){
+        $data = array(
+            'secret' => env('H_CAPTCHA_SECRET_KEY'),
+            'response' => $h_captcha_response
+        );
+        $verify = curl_init();
+        curl_setopt($verify, CURLOPT_URL, "https://hcaptcha.com/siteverify");
+        curl_setopt($verify, CURLOPT_POST, true);
+        curl_setopt($verify, CURLOPT_POSTFIELDS, http_build_query($data));
+        curl_setopt($verify, CURLOPT_RETURNTRANSFER, true);
+        $response = curl_exec($verify);
+        // var_dump($response);
+        $responseData = json_decode($response);
+
+//        return $responseData;
+
+        if($responseData->success) {
+            // your success code goes here
+            return true;
+        } else {
+            // return error to user; they did not pass
+            return false;
+        }
+    }
 
 
     public function generateCSRF($length = 28)
